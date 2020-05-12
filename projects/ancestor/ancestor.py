e219de1f323d12c2c404c9d9c730d4dc118ed338
nathan-saygers <nathan.saygers@gmail.com>
2020-05-12 13:44:25 -0700

    add build_graph helper

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,28 @@
 
+from util import *
+
+
 def earliest_ancestor(ancestors, starting_node):
     pass
+
+
+test_input = [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7),
+              (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+
+
+def build_graph(ancestors):
+    anc_graph = Graph()
+
+    for pair in ancestors:
+        if pair[0] not in anc_graph.vertices:
+            anc_graph.add_vertex(pair[0])
+        if pair[1] not in anc_graph.vertices:
+            anc_graph.add_vertex(pair[1])
+
+    for edge in ancestors:
+        anc_graph.add_edge(edge[0], edge[1])
+
+    return anc_graph
+
+
+print(build_graph(test_input).vertices)
